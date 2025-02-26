056fb8533abdce9c3975ecfaf8a5ed05292c1845
antonio2904 <40452468+antonio2904@users.noreply.github.com>
2025-02-26 16:54:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ tokenizer.pad_token = tokenizer.eos_token
 
 print(f"PAD Token: {tokenizer.pad_token}, PAD Token ID: {tokenizer.pad_token_id}")
 
-model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=5, torch_dtype="auto")
+model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=5, torch_dtype=torch.float16)
 model.config.pad_token_id = tokenizer.pad_token_id
 model.resize_token_embeddings(len(tokenizer))  # Ensure model recognizes new token
 
